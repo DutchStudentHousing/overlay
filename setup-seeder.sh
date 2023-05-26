05e150d5ad430fc58a33275ff304fd015493d866
Rick <117918276+Eranare@users.noreply.github.com>
2023-05-26 16:15:42 +0200

    revert setup-seeder.sh
    changed back

--- setup-seeder.sh
+++ setup-seeder.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 apt-get update -y
-apt-get install -y git debian-archive-keyring curl gnupg apt-transport-https
-mkdir -p /etc/apt/keyrings
+apt-get install debian-archive-keyring curl gnupg apt-transport-https git -y
+mkdir /etc/apt/keyrings
 curl -fsSL https://packagecloud.io/github/git-lfs/gpgkey | gpg --dearmor > /etc/apt/keyrings/github_git-lfs-archive-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/github_git-lfs-archive-keyring.gpg] https://packagecloud.io/github/git-lfs/debian buster main' > /etc/apt/sources.list.d/github_git-lfs.list
-echo 'deb-src [signed-by=/etc/apt/keyrings/github_git-lfs-archive-keyring.gpg] https://packagecloud.io/github/git-lfs/debian buster main' >> /etc/apt/sources.list.d/github_git-lfs.list
+echo <<EOF > /etc/apt/sources.list.d/github_git-lfs.list
+    deb [signed-by=/etc/apt/keyrings/github_git-lfs-archive-keyring.gpg] https://packagecloud.io/github/git-lfs/debian buster main
+    deb-src [signed-by=/etc/apt/keyrings/github_git-lfs-archive-keyring.gpg] https://packagecloud.io/github/git-lfs/debian buster main
+EOF
 apt-get update -y
-apt-get install -y git-lfs
+apt-get install git-lfs -y
