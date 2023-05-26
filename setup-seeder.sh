5cf7ea2e5cf3a9a40bee5cf63b92b17ac69118c0
Rick <117918276+Eranare@users.noreply.github.com>
2023-05-26 16:06:55 +0200

    Update setup-seeder.sh

--- setup-seeder.sh
+++ setup-seeder.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 apt-get update -y
-apt-get install debian-archive-keyring curl gnupg apt-transport-https git -y
-mkdir /etc/apt/keyrings
+apt-get install -y git debian-archive-keyring curl gnupg apt-transport-https
+mkdir -p /etc/apt/keyrings
 curl -fsSL https://packagecloud.io/github/git-lfs/gpgkey | gpg --dearmor > /etc/apt/keyrings/github_git-lfs-archive-keyring.gpg
-echo <<EOF > /etc/apt/sources.list.d/github_git-lfs.list
-    deb [signed-by=/etc/apt/keyrings/github_git-lfs-archive-keyring.gpg] https://packagecloud.io/github/git-lfs/debian buster main
-    deb-src [signed-by=/etc/apt/keyrings/github_git-lfs-archive-keyring.gpg] https://packagecloud.io/github/git-lfs/debian buster main
-EOF
+echo 'deb [signed-by=/etc/apt/keyrings/github_git-lfs-archive-keyring.gpg] https://packagecloud.io/github/git-lfs/debian buster main' > /etc/apt/sources.list.d/github_git-lfs.list
+echo 'deb-src [signed-by=/etc/apt/keyrings/github_git-lfs-archive-keyring.gpg] https://packagecloud.io/github/git-lfs/debian buster main' >> /etc/apt/sources.list.d/github_git-lfs.list
 apt-get update -y
-apt-get install git-lfs -y
-
-
+apt-get install -y git-lfs
